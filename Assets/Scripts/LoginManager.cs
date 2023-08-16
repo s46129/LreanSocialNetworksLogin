6c6f1de65795b7a7816c2eb8fa8ae8a7a98d5b1f
Rex <s46129@yahoo.com.tw>
2023-08-17 00:18:07 +0800

    feat : Add Login manager and button

---
+++ Assets/Scripts/LoginManager.cs
@@ -0,0 +1,36 @@
+using Line.LineSDK;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoginManager : MonoBehaviour
+{
+    [SerializeField] private Button lineButton;
+    [SerializeField] TextMeshProUGUI resultText;
+
+    private void Start()
+    {
+        lineButton.onClick.AddListener(LoginLine);
+    }
+
+
+    private void LoginLine()
+    {
+        var scopes = new string[] { "profile", "openid" };
+        LineSDK.Instance.Login(scopes, result =>
+        {
+            result.Match(
+                value =>
+                {
+                    resultText.text = "Login OK. User display name: " + value.UserProfile.DisplayName;
+                    Debug.Log("Login OK. User display name: " + value.UserProfile.DisplayName);
+                },
+                error =>
+                {
+                    resultText.text = "Login failed, reason: " + error.Message;
+                    Debug.Log("Login failed, reason: " + error.Message);
+                }
+            );
+        });
+    }
+}
