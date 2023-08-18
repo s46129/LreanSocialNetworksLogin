e7fab1b584dd3a88c6b202f7bc4967a5eef1673d
Rex <s46129@yahoo.com.tw>
2023-08-18 14:10:36 +0800

    feat : Add LoginUnitySocial login

--- Assets/Scripts/LoginManager.cs
+++ Assets/Scripts/LoginManager.cs
@@ -1,16 +1,19 @@
 using Line.LineSDK;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
 using UnityEngine.UI;
 
 public class LoginManager : MonoBehaviour
 {
     [SerializeField] private Button lineButton;
+    [SerializeField] private Button unitySocialButton;
     [SerializeField] TextMeshProUGUI resultText;
 
     private void Start()
     {
         lineButton.onClick.AddListener(LoginLine);
+        unitySocialButton.onClick.AddListener(LoginUnitySocial);
     }
 
 
@@ -32,6 +35,23 @@ public class LoginManager : MonoBehaviour
                     Debug.Log("Login failed, reason: " + error.Message);
                 }
             );
+        });
+    }
+
+    private void LoginUnitySocial()
+    {
+        Social.localUser.Authenticate(success =>
+        {
+            if (success)
+            {
+                resultText.text = "Login Unity Social success.";
+                Debug.Log("Login OK. User display name: " + Social.localUser.userName);
+            }
+            else
+            {
+                resultText.text = "Login Unity Social failed.";
+                Debug.Log("Login failed.");
+            }
         });
     }
 }
